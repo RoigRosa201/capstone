169b3f36f301777920029054d91fd3e884fa7429
Michael Pascuzzi <gatorpazz@gmail.com>
2023-03-20 19:28:36 -0400

    JavaScript intro

---
+++ block07/main.js
@@ -0,0 +1,43 @@
+// console.log("Hello world")
+// console.log("Hello world part 2")
+// console.log("Hello world part 3")
+// console.log("Hello world part 4")
+// console.log("Hello world part 5")
+const username = 'gatorpazz';
+console.log("Your username is " + username);
+
+console.log(name);
+
+const num1 = 5;
+const num2 = "8";
+
+// this will concatenate
+console.log(num1 + num2);
+
+console.log(10 * 5);
+console.log(11 / 2);
+console.log(6 ** 2);
+
+console.log(5 % 2);
+console.log(4 % 2);
+
+
+console.log("Michael" === "Thomas")
+console.log("Frank" === "Frank")
+console.log("Michael" !== "Elijah");
+
+console.log(5 == 5)
+console.log(5 === 5)
+
+console.log("5" == 5)
+console.log("5" === 5)
+
+console.log(5 > 9);
+console.log(10 <= 10);
+
+console.log(`
+  This is a big template!
+  This is the value of num1: ${num1}
+  This is the value of num2: ${num2}
+  This is the value of username: ${username}
+`)
