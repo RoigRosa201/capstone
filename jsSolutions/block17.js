b548b606f4871efdb70788f1c62025ed626daa6c
Thomas Pollick <thomaspollick@Thomass-MacBook-Pro-2.local>
2023-05-04 19:33:45 -0500

    change regular function to arrow function

--- jsSolutions/block17.js
+++ jsSolutions/block17.js
@@ -1,14 +1,14 @@
 const coffeeData = require("./block17-data.js")
-console.log(coffeeData)
+// console.log(coffeeData)
 
 //2. Print an array of all the drinks on the menu.
 const coffeeNames = coffeeData.map(drink => drink.name)
 
-console.log(coffeeNames)
+// console.log(coffeeNames)
 
 //3. Print an array of drinks that cost 5 and under.
 const cheapDrinks = coffeeData.filter(drink => drink.price <= 5)
-console.log(cheapDrinks)
+// console.log(cheapDrinks)
 
 //4. Print an array of drinks that are priced at an even number.
 const evenPrice = coffeeData.filter(drink => drink.price % 2 === 0)
@@ -20,10 +20,10 @@ console.log(totalPrice)
 
 //6. Print an array with all the drinks that are seasonal.
 const seasonalDrinks = coffeeData.filter(drink => drink.seasonal)
-console.log(seasonalDrinks)
+// console.log(seasonalDrinks)
 
 //7. Print all the seasonal drinks with the words "with imported beans" after the item name. For example: "affogato with imported beans".
 
 const seasonalWithBeans = seasonalDrinks.map(drink => drink.name + " with imported beans")
-console.log(seasonalWithBeans)
+// console.log(seasonalWithBeans)
 
