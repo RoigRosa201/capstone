e81e9af950bd10349231e92c6fbb48b30d808382
Thomas Pollick <thomaspollick@Thomass-MacBook-Pro-2.local>
2023-05-18 17:32:16 -0500

    block 18 and 19

---
+++ jsSolutions/block18.js
@@ -0,0 +1,36 @@
+//1. A function called "multiplication" that returns 
+//the product of the two input numbers.
+
+//Expect [action] to be [some result]
+
+//Expect multiplication(3,6) to be 18
+//Expect multiplication(5,5) to be a number
+//Expect multiplication('x', 5) to throw error
+//Expect multiplication(3, 5) to be 15
+//Expect multiplication(0, 5) to be 0
+//Expect multiplication(-5,5) to be -25
+//Expect multiplication(-5,-5) to be 25
+//Expect multiplication(undefined, 5) to throw error
+//Expect multiplication(null, 5) to throw error
+
+
+
+//2. Concat odds 
+
+//unexpected data types should be filtered out or throw an eror
+//If there are mulitple to the same values, only display one of those values in the resulting array
+
+
+//function tests
+
+//What should happen if the cart is empty?
+//Checkout button should be hidden - don't let user checkout
+
+//What needs to be shown to the user at each step of check out?
+//number of items in cart
+//displaying rest of the webpage
+//username in top right corner
+
+//What behaviors of this feature does the prompt miss or gloss over?
+//coupons, suggested products, items for purchase, total price, tax and shipping amount
+
